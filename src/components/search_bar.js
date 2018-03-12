8ad8423e19910601468a2d4eb77e1df0df139bd1
Jay Arteficio <jarteficio@gmail.com>
2018-03-12 15:44:48 -0700

    add prelim state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,21 +5,43 @@ import React, { Component } from 'react';
 // const Component = React.Component;
 
 class SearchBar extends Component {
-  render() {
-    return <input onChange={this.onInputChange} />;
+  constructor(props) {
+    super(props);
 
-    // create a new input element and pass it a prop 'onChange'
-    // with a value of the onInputChange
+    this.state = { term: '' };
   }
 
-  onInputChange (event) {
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={ event => this.setState({ term: event.target.value }) } />
+          Value of the input: { this.state.term }
+      </div>
+    );
     console.log(event.target.value);
   }
-  //this is the event handler
-
 }
 
 export default SearchBar;
+
+// simple verison with callback function
+
+// class SearchBar extends Component {
+//   render() {
+//     return <input onChange={this.onInputChange} />;
+//
+//     // create a new input element and pass it a prop 'onChange'
+//     // with a value of the onInputChange
+//   }
+//
+//   onInputChange (event) {
+//     console.log(event.target.value);
+//   }
+//   //this is the event handler
+//
+// }
 
 // class based component
 // import React from 'react';
